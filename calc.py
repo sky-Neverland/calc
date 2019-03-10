b8a6bd1b1c7b093a0e094e8c9f591f2eb88aa84c
sky-Neverland <48169106+sky-Neverland@users.noreply.github.com>
2019-03-10 22:08:56 +0800

    Update calc.py

--- calc.py
+++ calc.py
@@ -11,10 +11,10 @@ def calc():
 	op=['']*4	#oprators used in equations
 	equ=''		#equation
 
-	for i in range(n-1):								#randomize num & op
+	for i in range(n-1):					#randomize num & op
 		num[i]=random.randint(1,100)
 		op[i]=random.choice('+-*/')
-		equ+=str(num[i])+op[i]							#set up equations
+		equ+=str(num[i])+op[i]				#set up equations
 	num[n-1]=random.randint(1,100)
 	equ+=str(num[n-1])		
 
